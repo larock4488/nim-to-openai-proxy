1511587a6e067d341c9f30b5b5101e6326d23c8f
larock4488 <larock4488@gmail.com>
2026-09-09 07:00:03 -0400

    Update server.js kimi fix?

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ app.post('/v1/chat/completions', async (req, res) => {
         } else if (isMiniMaxM3) {
           baseRequest.chat_template_kwargs = { thinking_mode: "enabled" };
         } else if (isKimiK3) {
-          baseRequest.chat_template_kwargs = {"thinking":True, "clear_thinking":True, "do_sample":True, "enable_thinking":True, "reasoning_effort":"high"}
+          baseRequest.chat_template_kwargs = { thinking: true, clear_thinking: true, do_sample: true, enable_thinking: true, reasoning_effort: "high"}
         } else {
           baseRequest.chat_template_kwargs = { thinking: true };
         }
